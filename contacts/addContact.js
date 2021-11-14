b5ba3ba9c20e295c9f24ca8ec4db401967d4e5cf
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-11-14 16:40:55 +0100

    Create new files

---
+++ contacts/addContact.js
@@ -0,0 +1,19 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
+const { listContacts } = require("./listContacts");
+
+const contactsPath = path.resolve("");
+
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), ...data };
+  contacts.push(newContact);
+  await fs.writeFile(
+    `${contactsPath}/db/contacts.json`,
+    JSON.stringify(contacts)
+  );
+  return newContact;
+};
+
+module.exports = { addContact };
---
+++ contacts/listContacts.js
@@ -0,0 +1,12 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.resolve("");
+
+const listContacts = async () => {
+  const data = await fs.readFile(`${contactsPath}/db/contacts.json`);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+module.exports = { listContacts };
